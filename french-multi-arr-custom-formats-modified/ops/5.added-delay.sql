dbfa82029b68235a96eedf5ce47d6d4a1f154ea8
Tropicfront <86799660+Tropicfront@users.noreply.github.com>
2026-07-05 12:44:45 +0200

    Delete french-multi-arr-custom-formats-modified directory

--- french-multi-arr-custom-formats-modified/ops/5.added-delay.sql
+++
@@ -1,9 +0,0 @@
--- @operation: export
--- @entity: batch
--- @name: Added Delay
--- @exportedAt: 2026-05-18T22:00:45.093Z
--- @opIds: 501
-
--- --- BEGIN op 501 ( create delay_profile "Standard" )
-insert into "delay_profiles" ("name", "preferred_protocol", "usenet_delay", "torrent_delay", "bypass_if_highest_quality", "bypass_if_above_custom_format_score", "minimum_custom_format_score") values ('Standard', 'prefer_torrent', 0, 0, 0, 0, NULL);
--- --- END op 501
--- french-multi-arr-custom-formats-modified/ops/7.fix-movie-format.sql
+++
@@ -1,9 +0,0 @@
--- @operation: export
--- @entity: batch
--- @name: Fix Movie Format
--- @exportedAt: 2026-05-18T22:20:30.803Z
--- @opIds: 164
-
--- --- BEGIN op 164 ( update radarr_naming "Radarr" )
-update "radarr_naming" set "movie_format" = '{Movie CleanTitle} {(Release Year)} [tmdbid-{TmdbId}] - {Edition Tags }{[Custom Formats]}{[Quality Full]}{[MediaInfo 3D]}{[MediaInfo VideoDynamicRangeType]}{[Mediainfo AudioCodec}{ Mediainfo AudioChannels]}{[Mediainfo VideoCodec]}{-Release Group}' where "name" = 'Radarr' and "movie_format" = '{Movie CleanTitle} {(Release Year)} [tmdbid-{TmdbId}] - {Edition Tags }{[Custom Formats]}{[Quality Full]}{[MediaInfo 3D]}{[MediaInfo VideoDynamicRangeType]}{[Mediainfo AudioCodec}{ Mediainfo AudioChannels]}{[Mediainfo VideoCodec]}{-Release Group}{Movie CleanTitle}';
--- --- END op 164
